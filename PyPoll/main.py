8d6dd5d558ad6361ca3105420fd1ef860862cb1a
eyejayell <ijklloyd@gmail.com>
2023-01-18 14:20:55 -0500

    percentage added

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,9 +4,26 @@ election_path = "Resources/election_data.csv"
 
 election_df = pd.read_csv(election_path,encoding="utf-8")
 
+
 total_votes = election_df["Ballot ID"].count()
 
-vote_total_groups = election_df.groupby(["Candidate"])
+vote_total_groups = election_df.groupby(["Candidate"], as_index=False)
 
 vote_total_df = vote_total_groups.count()
-vote_total_df
+del vote_total_df["County"]
+#percentage formatting found here: https://stackoverflow.com/questions/51355282/add-percentage-column-to-panda-dataframe-with-percent-sign
+vote_total_df["Percentage"] = ((vote_total_df["Ballot ID"] / vote_total_df["Ballot ID"].sum()) * 100).round(3).astype(str) + "%"
+
+vote_total_df = vote_total_df[["Candidate","Percentage","Ballot ID"]]
+
+print(vote_total_df)
+
+print(vote_total_df.to_string(header=False,index=False))
+
+with open("output.txt","a") as file:
+    print("Election Results",file=file)
+    print("-------------------------",file=file)
+    print(f"Total Votes: {total_votes}",file=file)
+    print("-------------------------",file=file)
+    vote_total_text = vote_total_df.to_string(header=False, index=False)
+    file.write(vote_total_text)
